9988b4bb1bdc4b38c1ed60a9c1b676d3ef4b9dbb
bekkkdd <bekdauletushkempirov@gmail.com>
2021-05-12 20:14:34 +0600

    removed method realization

--- src/main/java/com/example/filter_lab/DBManager.java
+++ src/main/java/com/example/filter_lab/DBManager.java
@@ -31,26 +31,6 @@ public class DBManager {
 
         ArrayList<User> userList = new ArrayList<>();
 
-        try {
-
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM users join roles on users.role_id = roles.id");
-            ResultSet resultSet = statement.executeQuery();
-
-            while (resultSet.next()) {
-                Long id = resultSet.getLong("id");
-                String email = resultSet.getString("email");
-                String password = resultSet.getString("password");
-                String full_name = resultSet.getString("full_name");
-
-                userList.add(new User(id, email, password, full_name));
-            }
-
-            statement.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         return userList;
 
     }
@@ -58,46 +38,7 @@ public class DBManager {
     public static User getUserByEmailAndPassword(String email, String password){
         User user = null;
 
-        try{
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM users join roles on users.role_id = roles.id " +
-                    "where email = ? and password = ?");
-            statement.setString(1, email);
-            statement.setString(2, password);
-            ResultSet resultSet = statement.executeQuery();
-            while(resultSet.next()){
-                Long id = resultSet.getLong("id");
-                String full_name = resultSet.getString("full_name");
-
-                user = new User(id, email, password, full_name);
-            }
-            statement.close();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
         return user;
 
     }
-
-//    void updateUser (User user){
-//        try {
-//
-//            PreparedStatement statement = connection.prepareStatement("UPDATE" +
-//                    " users SET name = ?, city = ?, country = ?" +
-//                    " where id = " + user.getId()
-//            );
-//
-//            statement.setString(1, user.getName());
-//            statement.setString(2, user.getCity());
-//            statement.setString(3, user.getCountry());
-//
-//            statement.executeUpdate();
-//
-//            statement.close();
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-
 }
